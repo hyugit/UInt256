2464f53b0f0e8c4898ce48a1bc2e6423bc7049af
mryu87 <13056774+mryu87@users.noreply.github.com>
2017-12-30 19:08:12 -0500

    add macOS target, make carthage work

---
+++ Sources/UInt256.h
@@ -0,0 +1,7 @@
+@import Foundation
+
+//! Project version number for UInt256_iOS.
+FOUNDATION_EXPORT double UInt256VersionNumber;
+
+//! Project version string for UInt256_iOS.
+FOUNDATION_EXPORT const unsigned char UInt256VersionString[];
