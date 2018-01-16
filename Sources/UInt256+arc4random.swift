f40ffb39eb1d7b8db116f5f53febb9ac64704ddc
mryu87 <13056774+mryu87@users.noreply.github.com>
2018-01-15 20:59:42 -0800

    add proper uniform random number generator

--- Sources/UInt256+arc4random.swift
+++ Sources/UInt256+arc4random.swift
@@ -2,8 +2,23 @@
 
 import Foundation
 
+public func arc4random_uniform(_ upperBound: UInt8) -> UInt8 {
+    let result = arc4random_uniform(UInt32(upperBound))
+    return UInt8(truncatingIfNeeded: result)
+}
+
+public func arc4random_uniform(_ upperBound: UInt16) -> UInt16 {
+    let result = arc4random_uniform(UInt32(upperBound))
+    return UInt16(truncatingIfNeeded: result)
+}
+
 public func arc4random_uniform(_ upperBound: UInt64) -> UInt64 {
     let upperBoundHigh = UInt32(truncatingIfNeeded: (upperBound >> 32))
+
+    if upperBoundHigh == 0 {
+        let lowerHalf = UInt32(truncatingIfNeeded: upperBound)
+        return UInt64(arc4random_uniform(lowerHalf))
+    }
 
     func generateRandom(within highBits: UInt32) -> UInt64 {
         let high: UInt32 = arc4random_uniform(highBits + 1)
@@ -13,7 +28,29 @@ public func arc4random_uniform(_ upperBound: UInt64) -> UInt64 {
     }
 
     var result = generateRandom(within: upperBoundHigh)
-    while result > upperBound {
+    while result >= upperBound {
+        result = generateRandom(within: upperBoundHigh)
+    }
+
+    return result
+}
+
+public func arc4random_uniform(_ upperBound: UInt256) -> UInt256 {
+    let zeroPartCount = upperBound.leadingZeroBitCount / 64
+    let upperBoundHigh = upperBound[zeroPartCount]
+
+    func generateRandom(within highBits: UInt64) -> UInt256 {
+        var parts = Array(repeating: UInt64.min, count: zeroPartCount)
+        parts.append(arc4random_uniform(highBits + 1))
+
+        while parts.count < 4 {
+            parts.append(arc4random64())
+        }
+        return UInt256(parts)
+    }
+
+    var result = generateRandom(within: upperBoundHigh)
+    while result >= upperBound {
         result = generateRandom(within: upperBoundHigh)
     }
 
