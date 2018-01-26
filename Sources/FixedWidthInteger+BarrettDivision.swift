6808cdfde4cd47c3a9e7fd8acc60e9aaebdb8ee2
mryu87 <13056774+mryu87@users.noreply.github.com>
2018-01-26 12:56:40 -0800

    remove delegation

--- Sources/FixedWidthInteger+BarrettDivision.swift
+++ Sources/FixedWidthInteger+BarrettDivision.swift
@@ -2,36 +2,8 @@
 
 import Foundation
 
-public protocol FixedWidthIntegerBarrettDivisionDelegate {
-    var precomputedInverse: (high: Any, low: Any) { get set }
-}
-
-public protocol FixedWidthIntegerWithBarrettDivision: FixedWidthInteger {
-    var delegate: FixedWidthIntegerBarrettDivisionDelegate? { get set }
-    func dividingFullWidthBarrett(_ dividend: (high: Self, low: Self)) -> (quotient: Self, remainder: Self)
-    func dividingFullWidth(_ dividend: (high: Self, low: Self), withPrecomputedInverse: (high: Self, low: Self)) -> (quotient: Self, remainder: Self)
-}
-
 extension FixedWidthInteger {
-    public var delegate: FixedWidthIntegerBarrettDivisionDelegate? {
-        get {
-            return nil
-        }
-
-        set {}
-    }
-
-    public func dividingFullWidthBarrett(_ dividend: (high: Self, low: Self)) -> (quotient: Self, remainder: Self) {
-        if let high = self.delegate?.precomputedInverse.high as? Self,
-            let low = self.delegate?.precomputedInverse.low as? Self
-        {
-            return self.dividingFullWidth(dividend, withPrecomputedInverse: (high, low))
-        } else {
-            return self.dividingFullWidth((dividend.high, dividend.low.magnitude))
-        }
-    }
-
-    public func dividingFullWidth(_ dividend: (high: Self, low: Self), withPrecomputedInverse: (high: Self, low: Self)) -> (quotient: Self, remainder: Self) {
-        return self.dividingFullWidth((dividend.high, dividend.low.magnitude))
+    public func dividingFullWidth(_ dividend: (high: Self, low: Self.Magnitude), withPrecomputedInverse: (high: Self, low: Self)) -> (quotient: Self, remainder: Self) {
+        return self.dividingFullWidth(dividend)
     }
 }
