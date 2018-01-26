4aad68a7819b3c411b3edd68bca2689c4e901443
hyugit <13056774+hyugit@users.noreply.github.com>
2018-01-26 13:09:22 -0800

    cleanup

--- Sources/FixedWidthInteger+BarrettDivision.swift
+++
@@ -1,9 +0,0 @@
-//
-
-import Foundation
-
-extension FixedWidthInteger {
-    public func dividingFullWidth(_ dividend: (high: Self, low: Self.Magnitude), withPrecomputedInverse: (high: Self, low: Self)) -> (quotient: Self, remainder: Self) {
-        return self.dividingFullWidth(dividend)
-    }
-}
--- Sources/UInt256+BarrettDivision.swift
+++ Sources/UInt256+BarrettDivision.swift
@@ -2,6 +2,12 @@
 
 import Foundation
 
+extension FixedWidthInteger {
+    public func dividingFullWidth(_ dividend: (high: Self, low: Self.Magnitude), withPrecomputedInverse: (high: Self, low: Self)) -> (quotient: Self, remainder: Self) {
+        return self.dividingFullWidth(dividend)
+    }
+}
+
 extension UInt256 {
 
     public func dividingFullWidth(
