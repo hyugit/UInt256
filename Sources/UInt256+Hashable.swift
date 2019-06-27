aec35ef5a878bfb833e41c13dbfe01a83db81972
hyugit <13056774+hyugit@users.noreply.github.com>
2019-06-26 21:54:27 -0700

    fix hashable backward compatible

--- Sources/UInt256+Hashable.swift
+++ Sources/UInt256+Hashable.swift
@@ -4,4 +4,8 @@ extension UInt256: Hashable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(self.toHexString().hashValue)
     }
+
+    public var hashValue: Int {
+        return toHexString().hashValue
+    }
 }
