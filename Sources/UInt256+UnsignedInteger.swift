6e87f1d243ac63efb7ff069e4ed55ddccb0ca8cf
MarcoDotIO <Marcus.Arnett10@gmail.com>
2024-10-01 08:57:26 -0400

    fix: implement nonisolated value to UnsignedInteger extension

--- Sources/UInt256+UnsignedInteger.swift
+++ Sources/UInt256+UnsignedInteger.swift
@@ -4,7 +4,7 @@
 // see: https://developer.apple.com/documentation/swift/unsignedinteger
 extension UInt256: UnsignedInteger {
     
-    public static var max = UInt256([UInt64.max, UInt64.max, UInt64.max, UInt64.max])
+    nonisolated(unsafe) public static var max = UInt256([UInt64.max, UInt64.max, UInt64.max, UInt64.max])
     public static var min = UInt256([UInt64.min, UInt64.min, UInt64.min, UInt64.min])
 
     public var magnitude: UInt256 {
