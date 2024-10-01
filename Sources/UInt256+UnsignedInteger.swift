3dc471e8489ed2a1b16ac63c3bd243d97ac8478b
MarcoDotIO <Marcus.Arnett10@gmail.com>
2024-10-01 09:09:50 -0400

    fix: add nonisolated property to min

--- Sources/UInt256+UnsignedInteger.swift
+++ Sources/UInt256+UnsignedInteger.swift
@@ -5,7 +5,7 @@
 extension UInt256: UnsignedInteger {
     
     nonisolated(unsafe) public static var max = UInt256([UInt64.max, UInt64.max, UInt64.max, UInt64.max])
-    public static var min = UInt256([UInt64.min, UInt64.min, UInt64.min, UInt64.min])
+    nonisolated(unsafe) public static var min = UInt256([UInt64.min, UInt64.min, UInt64.min, UInt64.min])
 
     public var magnitude: UInt256 {
         return UInt256(parts)
