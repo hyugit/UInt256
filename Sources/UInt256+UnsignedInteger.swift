a5cd5756244550da79721986ef303cfad3987c75
MarcoDotIO <Marcus.Arnett10@gmail.com>
2025-02-13 23:11:58 -0500

    Update min and max in UnsignedInteger extension to support Swift 6.0 (#8)
    * fix: implement nonisolated value to UnsignedInteger extension
    
    * fix: add nonisolated property to min
    
    * Update UInt256+UnsignedInteger.swift
    
    * Update UInt256.swift

--- Sources/UInt256+UnsignedInteger.swift
+++ Sources/UInt256+UnsignedInteger.swift
@@ -4,8 +4,8 @@
 // see: https://developer.apple.com/documentation/swift/unsignedinteger
 extension UInt256: UnsignedInteger {
     
-    public static var max = UInt256([UInt64.max, UInt64.max, UInt64.max, UInt64.max])
-    public static var min = UInt256([UInt64.min, UInt64.min, UInt64.min, UInt64.min])
+    public static let max = UInt256([UInt64.max, UInt64.max, UInt64.max, UInt64.max])
+    public static let min = UInt256([UInt64.min, UInt64.min, UInt64.min, UInt64.min])
 
     public var magnitude: UInt256 {
         return UInt256(parts)
